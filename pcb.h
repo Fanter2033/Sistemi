1e24ae13ab34e2e76b1d0d87b4e4cb82c19db13e
Tiestaa <francesco.testa3@studio.unibo.it>
2023-01-25 22:09:36 +0100

    queue list functions ready

--- pcb.h
+++ pcb.h
@@ -1,28 +1,67 @@
 #ifndef PCB_H
 #define PCB_H
 
-#include "./pandos_types.h"
-#include "./pandos_const.h"
-#include "./list.h"
+#include "pandos_types.h"
+#include "pandos_const.h"
+#include "list.h"
 
-pcb_t pcbFree_table[MAXPROC];
+/* init, alloc and free */
 
-struct list_head pcbFree_h = LIST_HEAD_INIT(pcbFree_h);
+HIDDEN struct list_head pcbFree_h = LIST_HEAD_INIT(pcbFree_h);
 
-//1.
 void initPcbs(){
-    //inizializzazione p_list di ogni pcb (?)
-
+    static pcb_t pcbFree_table[MAXPROC];
     for (int i=0;i<MAXPROC;i++){
         list_add(&(pcbFree_table[i].p_list),&pcbFree_h);
     }
 }
 
-//2.
 void freePcb(pcb_t* p){
     list_add(&(p->p_list),&pcbFree_h);
 }
 
+pcb_t* allocPcb(){
+    if (list_empty(&pcbFree_h)) return NULL;
+    pcb_t* nodeToReturn = pcbFree_h.next;
+    list_del(pcbFree_h.next);
+    /*inizializzare tutti gli elementi a NULL, 0 */
+    return nodeToReturn;
+}
 
+/* Queue list management */
+void mkEmptyProcQ(struct list_head *head){
+    INIT_LIST_HEAD(head);
+}
 
-#endif 
+int emptyProcQ(struct list_head *head){
+    return list_empty(head);
+}
+
+void insertProcQ(struct list_head* head, pcb_t* p){
+    list_add_tail(p,head);
+}
+
+pcb_t* headProcQ(struct list_head* head){
+    if (list_empty(head)) return NULL;
+    return list_first_entry(head, struct pcb_t,p_list);
+}
+
+pcb_t* removeProcQ(struct list_head* head){
+    if (list_empty(head)) return NULL;
+    pcb_t* nodeToReturn = list_first_entry(head, struct pcb_t, p_list);
+    list_del(nodeToReturn);
+    return nodeToReturn;
+}
+
+pcb_t* outProcQ(struct list_head* head, pcb_t* p){
+    struct pcb_t* iterator=NULL;
+    list_for_each_entry(iterator, head, p_list){
+        if ( iterator == p ){
+            list_del(iterator);
+            return iterator;
+        }
+    }
+    return NULL;
+}
+
+#endif //PCB_H
