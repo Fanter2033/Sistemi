32248c02f8362cba61080fdc9cb5adcf6da682a5
Fanter2033 <pietro.sami@studio.unibo.it>
2023-01-31 16:51:28 +0100

    ash funzionante ?????

--- ash.h
+++ ash.h
@@ -4,40 +4,45 @@
 #include "pandos_types.h"
 #include "pandos_const.h"
 #include "hashtable.h"
+#include "pcb.h"
 
 static semd_t semd_table[MAXPROC];
-struct hlist_head semdFree_h = HLIST_HEAD_INIT;
+struct list_head semdFree_h = LIST_HEAD_INIT(semdFree_h);
 
 
-DEFINE_HASHTABLE(semd_h,5);
-//hash_init(semd_h);
-//unsigned bkt;
+DECLARE_HASHTABLE(semd_h,5);
 
 /* add PCB pointed by p in the SEMD blocked process with semAdd as key */
 int insertBlocked(int *semAdd, pcb_t *p){
 
     struct semd_t* iterator; 
-    //probabilmente si ferma qui perchè non riesce ad iterare. 
     hash_for_each_possible(semd_h,iterator,s_link,*semAdd){
         list_add_tail(p,&(iterator->s_procq));
+        addokbuf("aggiungo pcb a semaforo esistente   \n");
         return 0;
     }
-    //da qui in poi funziona
-    if (!hlist_empty(&semdFree_h)) {
-        semd_PTR nodeToAdd=semdFree_h.first;
-        nodeToAdd->s_key=semAdd;
-        INIT_LIST_HEAD(&(nodeToAdd->s_procq));
-        list_add_tail(p,&(nodeToAdd->s_procq));
-        hash_add(semd_h,nodeToAdd,*semAdd);
-        hlist_del(semdFree_h.first);
+
+    if (!list_empty(&semdFree_h)) {
+        semd_t* nodeToAdd = semdFree_h.next;
+        /* node initialization */
+        *(nodeToAdd->s_key) = *semAdd;
+        mkEmptyProcQ(&(nodeToAdd->s_procq));    
+        insertProcQ(&(nodeToAdd->s_procq),p);
+        
+        hash_add(semd_h,&(nodeToAdd->s_link), *(nodeToAdd->s_key));
+        list_del(semdFree_h.next);
+
+        addokbuf("aggiungo semaforo a ash\n");
         return 0;
     }
+
+    addokbuf("nulla da fare \n");
     return 1;
 }
 
 void initASH(){
     for (int i=0;i<MAXPROC;i++){
-        hlist_add_head(&(semd_table[i]),&semdFree_h);
+        list_add(&(semd_table[i]),&semdFree_h);
     }
 }
 
