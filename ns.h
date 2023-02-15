a14f3ffced3708e270facf32ef61b76c80650a1e
Fanter2033 <pietro.sami@studio.unibo.it>
2023-02-15 15:59:02 +0100

    niente

--- ns.h
+++ ns.h
@@ -11,14 +11,19 @@ HIDDEN struct list_head PID_nsFree_h = LIST_HEAD_INIT(PID_nsFree_h);
 /*Lista dei namespace attivi*/
 HIDDEN struct list_head PID_nsList_h = LIST_HEAD_INIT(PID_nsList_h);
 
+/* */
 void initNamespaces();
 
+/* */
 nsd_t* getNamespace(pcb_t *p, int type);
 
+/* */
 int addNamespace(pcb_t *p, nsd_t *ns);
 
+/* */
 nsd_t *allocNamespace(int type);
 
+/* */
 void freeNamespace(nsd_t *ns);
 
 
